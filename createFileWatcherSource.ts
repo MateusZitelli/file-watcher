2f7faad856f993c84ef9affdbd1a1e62d42d571b
Mateus Zitelli <zitellimateus@gmail.com>
2020-01-06 13:57:14 -0300

    Add diff

--- createFileWatcherSource.ts
+++ createFileWatcherSource.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs";
+import { TMP_FOLDER } from "./consts";
+import { exec } from "child_process";
 
-// Creates a callbag source that delivers filesystem events of a given folder
+// This is our events source, it will keep track of
 export const createFileWatcherSource = path => (start, sink) => {
   if (start !== 0) return;
 
@@ -16,15 +18,63 @@ export const createFileWatcherSource = path => (start, sink) => {
         watcher.close();
         console.log("Watcher closed");
       }
+
+      // Remove tmp dir
+      if (fs.existsSync(TMP_FOLDER)) {
+        fs.readdir(TMP_FOLDER, (err, files) => {
+          files.forEach(file => fs.unlinkSync(`${TMP_FOLDER}/${file}`))
+          fs.rmdirSync(TMP_FOLDER);
+        })
+      }
+    }
+  });
+
+  // Keep a copy of the original files
+  fs.readdir(path, (err, files) => {
+    if (err) {
+      sink(2, err);
+    } else {
+      if (!fs.existsSync(TMP_FOLDER)) {
+        fs.mkdirSync(TMP_FOLDER);
+      }
+
+      files.forEach(filename => {
+        fs.copyFileSync(`${path}/${filename}`, `${TMP_FOLDER}/${filename}`);
+        sink(1, { event: "found", filename });
+      });
     }
   });
 
   // Start watcher
   watcher = fs.watch(path, (event, filename) => {
-    // Send event to the sink if it is subscribed
+    // Continue if filename is available and sink is subscribed
     if (filename && !ended) {
-      sink(1, { event, filename });
+      // Keep a copy of new files
+      if (event === "rename") {
+        try {
+          // If the file exists the file was renamed
+          if (fs.existsSync(`${path}/${filename}`)) {
+            fs.copyFileSync(`${path}/${filename}`, `${TMP_FOLDER}/${filename}`);
+            sink(1, { event: "rename", filename });
+            // If the file does not exist it was removed
+          } else {
+            sink(1, { event: "delete", filename });
+          }
+        } catch (e) {
+          console.error(`Fail to create ${TMP_FOLDER}/${filename}`);
+        }
+      } else if (event === "change") {
+        exec(
+          `diff ${path}/${filename} ${TMP_FOLDER}/${filename}`,
+          (err, stdout, stderr) => {
+            sink(1, { event: "change", filename, diff: stdout });
+          }
+        );
+      } else {
+        sink(1, { event, filename });
+      }
     }
   });
+
   console.log("Watcher started");
 };
--- readme.ts
+++ readme.ts
@@ -1,8 +1,8 @@
 /*
 Event-broker implementation based on the Callbags specs.
 
-This will watch changes happening in the ./logs folder, logging the name of the first changed file
-and also logging the first 5 change events. After all subscribers finish the watcher is closed.
+This will watch changes happening in the ./logs folder, It will log the first 10 change in files inside the folder.
+Then it will clear tmp data and stop the watcher.
 
 To use this run:
 $ yarn start
@@ -12,33 +12,104 @@ And change files inside the ./logs folder.
 The expected output is something like:
 
 Watcher started
-foo just changed.
-{ event: 'rename', filename: 'foo' }
-{ event: 'change', filename: 'foo' }
-{ event: 'rename', filename: 'bar' }
-{ event: 'change', filename: 'bar' }
-{ event: 'change', filename: 'bar' }
+filename: foo
+event: rename
+timestamp: 2020-01-06T16:55:13.446Z
+########
+filename: foo
+event: change
+timestamp: 2020-01-06T16:55:13.457Z
+########
+filename: bar
+event: rename
+timestamp: 2020-01-06T16:55:15.754Z
+########
+filename: bar
+event: change
+timestamp: 2020-01-06T16:55:15.762Z
+########
+filename: foo
+event: change
+timestamp: 2020-01-06T16:55:26.590Z
+diff:
+1d0
+< hey there
+
+########
+filename: foo
+event: change
+timestamp: 2020-01-06T16:55:26.592Z
+diff:
+1d0
+< hey there
+
+########
+filename: bar
+event: change
+timestamp: 2020-01-06T16:55:39.791Z
+diff:
+1d0
+< hello
+
+########
+filename: bar
+event: change
+timestamp: 2020-01-06T16:55:39.793Z
+diff:
+1d0
+< hello
+
+########
+filename: bar
+event: change
+timestamp: 2020-01-06T16:55:49.314Z
+diff:
+1d0
+< hello
+
+########
+filename: foo
+event: change
+timestamp: 2020-01-06T16:55:52.335Z
+diff:
+1d0
+< hey there
+
+########
 Watcher closed
 */
 
-import { take, pipe, observe, share, map } from "./operators";
-import { createFileWatchSource } from "./createFileWatchSource";
+import {
+  pipe,
+  observe,
+  map,
+  take
+} from "./operators";
+import { createFileWatcherSource } from "./createFileWatcherSource";
 
-const logsChangesSource = createFileWatchSource("./logs");
+const path = "./logs";
 
-// This will handle multiple subscribers to share the same source
-const sharedSource = share(logsChangesSource);
+const folderChangesSource = createFileWatcherSource(path);
 
-// Create our observable logger sink
-const observableLogger = observe(message => console.log(message));
-
-// Let's log the first file change
 pipe(
-  sharedSource,
-  take(1),
-  map(event => `${event.filename} just changed.`),
-  observableLogger
-);
+  folderChangesSource,
+  // Add timestamp
+  map(event => ({ 
+    timestamp: new Date().toISOString(), 
+    ...event 
+  })),
+  // Take the first 10 events
+  take(10),
 
-// And the first 5 file change events - after this the execution should finish
-pipe(sharedSource, take(5), observableLogger);
+  // Log
+  observe(({event, filename, timestamp, diff}) => {
+    console.log(`filename: ${filename}`)
+    console.log(`event: ${event}`)
+    console.log(`timestamp: ${timestamp}`)
+    if (diff) {
+      console.log(`diff:`);
+      console.log(`${diff}`);
+    }
+    console.log("########")
+  }),
+);
